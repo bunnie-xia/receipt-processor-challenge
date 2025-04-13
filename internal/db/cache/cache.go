cb32a3945083e6ad8d8a46710eae537ebd821e8f
Carmen <xiawenwu22@gmail.com>
2025-04-13 01:28:32 -0400

    Finished draft.

--- internal/db/cache/cache.go
+++ internal/db/cache/cache.go
@@ -3,35 +3,18 @@ package cache
 import (
 	"project/internal/db/receipts"
 	"reflect"
-	// "encoding/json"
 	"fmt"
-	// "string"
 	"strings"
   "strconv"
   "math"
-	// "errors"
 	"github.com/google/uuid"
 )
 
-// type Cache struct {
-// 	id string
-// 	objData interface {} receipts.processReceipt
-// 	Points float64 receipts.calPoints
-// }
 
-// type idGenFunc func(data *interface{}) string
-// type receipt.convertReceipt func(JSONreceipt []byte(jsonString)) struct
-
-// func generateId(data *interface{}) string {
-//   newUUID := uuid.new()
-//   data.id = newUUID
-// }
-
-// var CacheMap = make(map[string]receipts.Receipt)
 var CacheMap = make(map[interface{}]receipts.Receipt)
 var CachePoints = make(map[string]int)
 
-var TotalPrice float64 = 0.00
+var calculatedTotal float64 = 0.00
 
 func Set(receipt receipts.Receipt) string {
 	for key, cached := range CacheMap {
@@ -42,7 +25,6 @@ func Set(receipt receipts.Receipt) string {
 
 	id := uuid.New().String()
 	CacheMap[id] = receipt
-	// CachePoints[id] = CalculatePoints(id)
 
 	return id
 }
@@ -62,10 +44,7 @@ func getCachedReceipt(id string) receipts.Receipt {
 }
 
 
-
-
 func getItemInfo(items []receipts.Items) [][]string {
-// func getItemInfo(items [][]string) [][]string {
 	res := [][]string{}
 
 	for _, item := range items {
@@ -73,42 +52,32 @@ func getItemInfo(items []receipts.Items) [][]string {
 		price := item.Price
 
 		res = append(res, []string {descrip, price})
-		// res[0] = append(res[0], descrip)
-		// res[1] = append(res[1], price)
 
 		priceFloat, err := strconv.ParseFloat(price, 64)
 		if err != nil {
 			fmt.Println("Error parsing price:", err)
 			continue
 		}
-		TotalPrice += priceFloat
+		calculatedTotal += priceFloat
 	}
 
 	return res
 }
 
-func getTotalPrice(id string) float64 {
-	receipt := getCachedReceipt(id)
-	total := TotalPrice
-	if int(receipt.Total) == 0 {
-		TotalPrice = float64(0)
-		return math.Round(total * 100)/ 100
-		// receipt.Total = total
 
+func roundedRcptTotal(id string) string {
+	receipt := getCachedReceipt(id)
+	if receipt.Total == "" {
+		return "0.01"
 	}
 
-	return total
+	total, err := strconv.ParseFloat(receipt.Total, 64)
+	if err != nil {
+			fmt.Println("Error parsing total to float:", err)
+			return "0.01"
+	}
+
+	roundedFloat := math.Round(total * 100) / 100
+	floatStr := strconv.FormatFloat(roundedFloat, 'f', 2, 64)
+	return floatStr
 }
-// func getPoints(id string) {
-// 	if id == "" {
-// 		fmt.Println("Receipt id not found.")
-// 		return
-// 	}
-
-// 	points, exist := Cache[id]
-// 	if !exists {
-// 		return errors.New("Item not found.")
-// 	}
-
-// 	return object.id.Points.sum
-// }
